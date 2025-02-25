a4000608dc021b7bf563aa69acbc25736b0fde77
maryregan <mrr5943@psu.edu>
2025-02-25 12:37:43 -0500

    update

--- counter-app.js
+++ counter-app.js
@@ -42,17 +42,28 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
         ...super.properties,
         
         count: { type: Number},
+        min: {type: Number, reflect: true},
+        max: {type: Number, reflect: true},
       };
     }
       // Lit scoped styles
     static get styles() {
       return [super.styles,
       css`
+        :host([count = "18"]) {
+          color: var(--ddd-theme-default-athertonViolet);
+        }
+        :host([count = "21"]) {
+          color: var(--ddd-theme-default-athertonViolet);
+        }
         :host {
           display: block;
           color: var(--ddd-theme-primary);
           background-color: var(--ddd-theme-accent);
           font-family: var(--ddd-font-navigation);
+        }
+        :host([count="10"]) {
+          color: var(--ddd-theme-default-athertonViolet);
         }
         .wrapper {
           margin: var(--ddd-spacing-2);
@@ -63,18 +74,71 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
         }
       `];
     }
+   
     // Lit render the HTML
       render() {
         return html`
-      <div class="wrapper">
-      <div class="counter">{this.count}</div>
+      <confetti-container id="confetti"></confetti-container>
+      <div class="counter">${this.count}</div>
       <div class ="button">
-        <button>-1</button>
-        <button>+1</button>
+        <button @click="${this.decrease}" ?disabled="${this.min === this.count}">-1</button>
+        <button @click="${this.increase}" ?disabled="${this.max === this.count}">+1</button>
+        <button @click="${this.reset}">Reset</button>
       </div>
-    </div>`;
+    </div>
+    </confetti-container>`;
       }
 
+      increase() {
+        this.count++;
+      }
+      decrease() {
+        this.count--;
+      }
+      reset(){
+        this.count = 0;
+      }
+      
+
+      
+      updated(changedProperties) {
+        if (super.updated) {
+          super.updated(changedProperties);
+        }
+        if (changedProperties.has('count')) {
+          // do your testing of the value and make it rain by calling makeItRain
+          if (this.count === this.max || this.count === this.min) {
+            this.style.setProperty('--counter-color', 'var(--ddd-theme-default-athertonViolet)');
+          }
+          else {
+            this.style.setProperty('--counter-color', 'var(--ddd-theme-primary)');
+          }
+          if (this.count === 21)
+          {
+            this.makeItRain();
+          }
+          
+        }
+      }
+      makeItRain() {
+        // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+        // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+        // will only run AFTER the code is imported and available to us
+        import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+          (module) => {
+            // This is a minor timing 'hack'. We know the code library above will import prior to this running
+            // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+            // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+            // it's listening for changes so it can react
+            setTimeout(() => {
+              // forcibly set the poppped attribute on something with id confetti
+              // while I've said in general NOT to do this, the confetti container element will reset this
+              // after the animation runs so it's a simple way to generate the effect over and over again
+              this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+            }, 0);
+          }
+        );
+      }
   /**
    * haxProperties integration via file reference
    */
